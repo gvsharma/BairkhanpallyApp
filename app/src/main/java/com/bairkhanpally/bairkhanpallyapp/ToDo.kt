5fea0e2c5207f9b01320d14a09a8f421a0bd9bfe
Gv Sharma <gvsharma4@gmail.com>
2019-03-09 22:53:16 +0530

    implementation URLs

---
+++ app/src/main/java/com/bairkhanpally/bairkhanpallyapp/ToDo.kt
@@ -0,0 +1,15 @@
+package com.bairkhanpally.bairkhanpallyapp
+
+//TODO Room database
+//https://medium.com/mindorks/room-kotlin-android-architecture-components-71cad5a1bb35
+
+//https://medium.com/mindorks/android-architecture-components-room-and-kotlin-f7b725c8d1d
+
+//TODO Jetpack
+//https://android.jlelse.eu/what-is-android-jetpack-737095e88161
+
+//https://developer.android.com/jetpack
+
+//implementation 'androidx.core:core-ktx:1.0.0'
+
+//https://www.raywenderlich.com/5376-introduction-to-android-jetpack
