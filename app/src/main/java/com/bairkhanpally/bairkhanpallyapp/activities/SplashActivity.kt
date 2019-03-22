3b3a2f872a5755f93e3d45525beb5efd5a2d2ad1
Gv Sharma <gvsharma4@gmail.com>
2019-03-22 23:16:54 +0530

    added firebase analytics

--- app/src/main/java/com/bairkhanpally/bairkhanpallyapp/activities/SplashActivity.kt
+++ app/src/main/java/com/bairkhanpally/bairkhanpallyapp/activities/SplashActivity.kt
@@ -4,15 +4,27 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bairkhanpally.bairkhanpallyapp.R
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class SplashActivity : AppCompatActivity() {
-
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        logEvent()
         Thread.sleep(2000)
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    fun logEvent() {
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "100")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Splash Screen")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "Splash Screen")
+        mFirebaseAnalytics?.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
